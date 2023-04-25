2ceedeb65379aea9ee36f8fbff4d3eee044933b2
Maria Fernanda Rojas <mariaf.rojash@utadeo.edu.co>
2023-04-25 11:45:47 -0500

    Ejercicios realizados en bootcamp AWS Restart con python

---
+++ Ejercicio3.py
@@ -0,0 +1,24 @@
+myString="Esto es un string"
+print(myString)
+print(type(myString))
+print(myString + " is of the data type " + str(type(myString)))
+
+'''
+Concatenar
+'''
+firstString = "water"
+secondString = " fall"
+thirdString = firstString + secondString
+print(thirdString)
+
+'''
+Cadenas de entrada
+'''
+name=input("¿Cual es tu nombre? ")
+print(name)
+
+"Cadenas de salida"
+color = input("What is your favorite color?  ")
+animal = input("What is your favorite animal?  ")
+print("{}, you like a {} {}!".format(name,color,animal))
+
---
+++ Ejercicio4.py
@@ -0,0 +1,35 @@
+myFruitList =["Apple","Banana","Cherry"]
+print(myFruitList)
+print(type(myFruitList))
+
+"Lista de posiciones"
+print(myFruitList[0])
+print(myFruitList[1])
+print(myFruitList[2])
+
+"Valores en una lista"
+myFruitList[2]="Orange"
+print(myFruitList)
+
+"Tupla"
+myFinalAnswerTuple = ("apple", "banana", "pineapple")
+print(myFinalAnswerTuple)
+print(type(myFinalAnswerTuple))
+
+print(myFinalAnswerTuple[0])
+print(myFinalAnswerTuple[1])
+print(myFinalAnswerTuple[2])
+
+"Diccionario de lista"
+myFavoriteFruitDictionary = {
+  "Akua" : "apple",
+  "Saanvi" : "banana",
+  "Paulo" : "pineapple"
+}
+print(myFavoriteFruitDictionary)
+print(type(myFavoriteFruitDictionary))
+
+print(myFavoriteFruitDictionary["Akua"])
+print(myFavoriteFruitDictionary["Saanvi"])
+print(myFavoriteFruitDictionary["Paulo"])
+
